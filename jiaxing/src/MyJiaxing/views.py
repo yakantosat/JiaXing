6dea8f9804071f609e46356046ea26ccdab3ebfa
pauline <yakantosat@gmail.com>
2014-12-20 22:11:44 +0800

    update JiaXing Project

---
+++ jiaxing/src/MyJiaxing/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
+import datetime
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("Hello, world")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
---
+++ jiaxing/src/jiaxing/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from MyJiaxing.views import hello, current_datetime, hours_ahead
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'jiaxing.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^hello$', hello),
+    (r'^current_time$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead)
+)
